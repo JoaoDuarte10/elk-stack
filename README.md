8874b8b5cdc8de963e6c2048e46a1ca7e74b26d8
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-05-23 21:42:47 -0300

    feat: create infra elk

---
+++ README.md
@@ -0,0 +1,5 @@
+Antes de criar os containeres em uma nova pasta, deve - se rodar o script `pre-compose.sh`, e caso precise, dê a permissão ao arquivo antes:
+
+```sh
+sudo chmod +x pre-compose.sh
+```
---
+++ pre-compose.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+echo -e " \033[0;32m Criando pasta data/elasticsearch/ \033[0m"
+mkdir data/elasticsearch/
+
+echo -e " \033[0;32m Dando permissão para pasta data/elasticsearch/ \033[0m"
+sudo chmod +x data/elasticsearch/
