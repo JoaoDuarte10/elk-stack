f00816bab5a9dd945ca95811e2834183ffb95816
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-08-14 00:30:45 -0300

    refactor: create script for prepare files data

--- pre-compose.sh
+++ pre-compose.sh
@@ -1,7 +1,16 @@
 # !/bin/bash
 
+echo -e " \033[0;32m Criando pasta data/ \033[0m"
+mkdir data/
+
 echo -e " \033[0;32m Criando pasta data/elasticsearch/ \033[0m"
 mkdir data/elasticsearch/
 
+echo -e " \033[0;32m Criando pasta data/grafana/ \033[0m"
+mkdir data/grafana/
+
 echo -e " \033[0;32m Dando permissão para pasta data/elasticsearch/ \033[0m"
 sudo chmod +x data/elasticsearch/
+
+echo -e " \033[0;32m Dando permissão para pasta data/grafana/ \033[0m"
+sudo chmod +x data/grafana/
